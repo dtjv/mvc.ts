7e32c14c238b6b8e981eca372cdddfb8ae009856
David Valles <davidtjvalles@gmail.com>
2021-01-30 17:56:38 -0800

    Add methods to model class

--- src/todos.ts
+++ src/todos.ts
@@ -1,10 +1,45 @@
+import cuid from 'cuid'
 import { Todo } from './types'
 
 export class Todos {
-  private readonly todos: Todo[] = []
+  private todos: Todo[] = []
 
-  insert(todo: Todo) {
+  insert(props: Pick<Todo, 'task'> & Partial<Todo>): void {
+    if (!props.task) {
+      throw new Error(`No task. Todo 'task' is required. Insert failed.`)
+    }
+
+    const todo: Todo = {
+      id: props.id ?? cuid(),
+      task: props.task,
+      done: props.done ?? false,
+    }
+
     this.todos.push(todo)
+  }
+
+  update(props: Pick<Todo, 'id'> & Partial<Todo>): void {
+    if (!props.id) {
+      throw new Error(`No id. Todo 'id' is required. Update failed.`)
+    }
+
+    this.todos = this.todos.map((todo) =>
+      todo.id === props.id
+        ? {
+            id: todo.id,
+            task: props.task ?? todo.task,
+            done: props.done ?? todo.done,
+          }
+        : todo
+    )
+  }
+
+  remove(todoId: string): void {
+    if (!todoId) {
+      throw new Error(`No id. Todo 'id' is required. Remove failed.`)
+    }
+
+    this.todos = this.todos.filter((todo) => todo.id !== todoId)
   }
 
   toJSON() {
