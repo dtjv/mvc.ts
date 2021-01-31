5301508747ae4d8e4d90de9d60881568975e17f5
David Valles <davidtjvalles@gmail.com>
2021-01-31 13:17:21 -0800

    Register handlers with view

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Todo } from './types'
 import { Todos } from './todos'
-import { View } from './view'
+import { View, ViewEvents } from './view'
 import { Store } from './store'
 
 export class App {
@@ -10,6 +10,21 @@ export class App {
     private readonly store: Store
   ) {
     this.load()
+
+    this.view.registerHandler({
+      event: ViewEvents.CREATE_TODO,
+      handler: this.handleCreateTodo.bind(this),
+    })
+
+    this.view.registerHandler({
+      event: ViewEvents.UPDATE_TODO,
+      handler: this.handleUpdateTodo.bind(this),
+    })
+
+    this.view.registerHandler({
+      event: ViewEvents.REMOVE_TODO,
+      handler: this.handleRemoveTodo.bind(this),
+    })
   }
 
   load() {
@@ -22,7 +37,31 @@ export class App {
     this.store.set(this.todos.toJSON())
   }
 
-  start() {
+  show() {
     this.view.render(this.todos.toJSON())
+  }
+
+  handleCreateTodo(props: Partial<Todo>): void {
+    if (props.task) {
+      this.todos.insert({ task: props.task })
+      this.save()
+      this.show()
+    }
+  }
+
+  handleUpdateTodo(props: Partial<Todo>): void {
+    if (props.id) {
+      this.todos.update({ ...props, id: props.id })
+      this.save()
+      this.show()
+    }
+  }
+
+  handleRemoveTodo(props: Partial<Todo>): void {
+    if (props.id) {
+      this.todos.remove(props.id)
+      this.save()
+      this.show()
+    }
   }
 }
