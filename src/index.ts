7b40d4e5bfee332c2631856c7b8449143e7efb58
David Valles <davidtjvalles@gmail.com>
2021-01-30 18:02:35 -0800

    Remove test data

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,11 @@
-import cuid from 'cuid'
-
-import { Todo } from './types'
 import { App } from './app'
 import { View } from './view'
 import { Todos } from './todos'
 import { Store } from './store'
 
-const view = new View()
+const view = new View(window.document, 'app')
 const todos = new Todos()
 const store = new Store('todos', localStorage)
 const app = new App(todos, view, store)
-
-const data: Todo[] = [
-  {
-    id: cuid(),
-    task: 'go for a run',
-    done: false,
-  },
-  {
-    id: cuid(),
-    task: 'buy bread',
-    done: false,
-  },
-  {
-    id: cuid(),
-    task: 'lift weights',
-    done: true,
-  },
-]
-
-store.set(data)
 
 app.start()
