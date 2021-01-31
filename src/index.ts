9ffaa88d0ee0771e9d674be53c868b071fb4a0f3
David Valles <davidtjvalles@gmail.com>
2021-01-31 13:18:02 -0800

    Pass id with # to view

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { View } from './view'
 import { Todos } from './todos'
 import { Store } from './store'
 
-const view = new View(window.document, 'app')
+const view = new View(window.document, '#app')
 const todos = new Todos()
 const store = new Store('todos', localStorage)
 const app = new App(todos, view, store)
