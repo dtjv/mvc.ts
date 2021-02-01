e711dc1ccd59beb030e6f204d67a38f22c3dd0b1
David Valles <davidtjvalles@gmail.com>
2021-02-01 14:24:58 -0800

    Make models extend Emitter

--- src/v2/todos.ts
+++ src/v2/todos.ts
@@ -1,53 +1,76 @@
 import cuid from 'cuid'
 
-export type Todo = {
+import { Emitter } from './emitter'
+
+export enum TodoEvents {
+  CHANGE = 'CHANGE',
+}
+
+type TodoProps = {
   id: string
   task: string
   done: boolean
 }
 
-export class Todos {
+export class Todo extends Emitter {
+  public readonly id: string
+  public task: string
+  public done: boolean
+
+  constructor(props: Pick<TodoProps, 'task'> & Partial<TodoProps>) {
+    super()
+
+    if (!props.task) {
+      throw new Error(`No task value. 'task' is required. Constructor failed.`)
+    }
+
+    this.id = props.id ?? cuid()
+    this.task = props.task
+    this.done = props.done ?? false
+  }
+
+  public toggle() {
+    this.done = !this.done
+    this.emit(TodoEvents.CHANGE)
+  }
+
+  public update(task: string) {
+    if (!task) {
+      throw new Error(`No task value. 'task' is required. Update failed.`)
+    }
+
+    if (this.task !== task) {
+      this.task = task
+      this.emit(TodoEvents.CHANGE)
+    }
+  }
+
+  public toJSON() {
+    return { id: this.id, task: this.task, done: this.done }
+  }
+}
+
+export class Todos extends Emitter {
   private todos: Todo[] = []
 
-  insert(props: Pick<Todo, 'task'> & Partial<Todo>): void {
-    if (!props.task) {
-      throw new Error(`No task. Todo 'task' is required. Insert failed.`)
-    }
-
-    const todo: Todo = {
-      id: props.id ?? cuid(),
-      task: props.task,
-      done: props.done ?? false,
-    }
-
+  public insert(todo: Todo): void {
+    todo.addListener(TodoEvents.CHANGE, this.onChange.bind(this))
     this.todos.push(todo)
   }
 
-  update(props: Pick<Todo, 'id'> & Partial<Todo>): void {
-    if (!props.id) {
-      throw new Error(`No id. Todo 'id' is required. Update failed.`)
-    }
-
-    this.todos = this.todos.map((todo) =>
-      todo.id === props.id
-        ? {
-            id: todo.id,
-            task: props.task ?? todo.task,
-            done: props.done ?? todo.done,
-          }
-        : todo
-    )
-  }
-
-  remove(todoId: string): void {
+  public remove(todoId: string): void {
     if (!todoId) {
-      throw new Error(`No id. Todo 'id' is required. Remove failed.`)
+      throw new Error(`No id value. 'id' is required. Remove failed.`)
     }
 
     this.todos = this.todos.filter((todo) => todo.id !== todoId)
   }
 
-  toJSON() {
-    return this.todos
+  public toJSON() {
+    return this.todos.map((todo) => todo.toJSON())
+  }
+
+  private onChange() {
+    this.emit(TodoEvents.CHANGE)
   }
 }
